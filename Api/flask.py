588a133656aeec0b3cc060ca3286649a74b5e5f9
shinsangeun <s0813se@naver.com>
2019-09-08 01:27:28 +0900

    Flask API를 이용한 req,res 샘플 코드 작성

---
+++ Api/flask.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_restful import reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+class RegistUser(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('value', type=int)
+        parser.add_argument('result',type=str)
+        args = parser.parse_args()
+
+        value = args['value']
+        result = args['result']
+
+        if result is not None :
+            return {'Message': "Success", 'code': 0, "Result": result}
+        else:
+            return {'Message': "error", 'code': 500}
+
+api.add_resource(RegistUser, '/ai/result')
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
