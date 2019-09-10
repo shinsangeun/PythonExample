632d05a7c2277bafd79353e35334f3dfb5315c73
shinsangeun <s0813se@naver.com>
2019-09-10 23:53:00 +0900

    혼자서 공부하는 파이썬-예제 코드 작성1

---
+++ StudyExample/output.py
@@ -0,0 +1,14 @@
+print("# 하나만 출력 합니다")
+print("Hello python")
+print()
+
+print("#여러개 출력")
+print(10,20,30,40,50)
+print("안녕","하세여")
+print()
+
+print("#아무것도 출력 하지 않아")
+print("-- 확인 전용 선 --")
+print()
+print()
+print("-- 확인 전용 선 --")
